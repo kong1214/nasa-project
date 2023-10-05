be9b83460d50864bfc70dc29e6371d5b0f927e48
kong1214 <kevin.ong@Live.com>
2023-10-05 11:55:09 -0700

    connected database

---
+++ backend/src/api/users.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
+
+mongoose
+  .connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => console.log(error));
+
+  require("../userDetails");
+  const User = mongoose.model("Users")
+
+
+  router.get('/', (req, res) => {
+
+    res.json(['😀', '😳', '🙄']);
+  });
+
+  router.post("/signup", async (req, res) => {
+    const { fname, lname, email, password } = req.body;
+
+    const encryptedpassword = await bcrypt.hash(password, 10);
+    try {
+      const oldUser = await User.findOne({ email });
+
+      if (oldUser) {
+        return res.send({ error: "User already exists with that email" });
+      }
+      await User.create({
+        fname: fname,
+        lname: lname,
+        email: email,
+        password: encryptedpassword,
+      });
+      res.send({ status: "ok" });
+    } catch (error) {
+      res.send({ status: error });
+    }
+  });
+
+module.exports = router;
